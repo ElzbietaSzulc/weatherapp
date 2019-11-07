d399fe1d93fe0b3f633c546030d65ddb11e66e74
Elżbieta Szulc <elzbieta.szulc@monterail.com>
2019-11-07 22:20:40 +0100

    Add js file

---
+++ app.js
@@ -0,0 +1,59 @@
+// Select elements
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+//App data
+const weather = {};
+
+weather.temperature = {
+    unit: "celsius"
+}
+
+//App const and vars
+
+const Kelvin = 273;
+const key = "2282772f730e9f35f3daf057777f7633";
+
+//Check if browser supports geolocation
+if('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't support Geolocation</p>";
+}
+}
+
+//Set User's Position
+
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    gwtWeather(latitude, longitude);
+}
+
+// show error when there is an issue with geolocation service
+
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`
+}
+
+//Get weather from api provider
+
+function getWeather(latitude, logitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?
+    lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch{api}
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weahter.temperature.value = Math.floor(data.main.temp - KELVIN);
+        })
+}
