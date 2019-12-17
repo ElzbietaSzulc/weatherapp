fa92c6ae6ffa244e9ef2950d8959f1073ec22e0e
Elżbieta Szulc <elzbieta.szulc@monterail.com>
2019-12-17 22:26:49 +0100

    Add changes to index.html and style.css files

--- tests/functions.js
+++ tests/functions.js
@@ -1,4 +1,4 @@
 const functions = {
-    celsiusToFarenheit: (temperature) => (temperature * 9) / 5 + 32
+    celsiusToFarenheit: (temperature) => (temperature * 9) / 5 + 32,
 }
 module.exports = functions;
