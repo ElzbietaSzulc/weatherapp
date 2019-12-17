39d93ae5042d2fe569407d7e5b1e58df633b0ef5
Elżbieta Szulc <elzbieta.szulc@monterail.com>
2019-12-17 20:02:13 +0100

    Add next test to functions.test file

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -1,5 +1,8 @@
 const functions = require("./functions");
 
 test('Celsius to Farenheit convertion', () => {
+  expect(functions.celsiusToFarenheit()).toBeDefined();
+});
+test('Celsius to Farenheit convertion', () => {
   expect(functions.celsiusToFarenheit(0)).toBe(32);
 });
